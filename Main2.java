a8bf6862b1cdb62e8b48619f3aaec7f9e94be594
Stepan <sud_pravo@bk.ru>
2023-06-19 16:05:51 +0700

    Второе  задание

---
+++ Main2.java
@@ -0,0 +1,23 @@
+public class Main2 {
+    public static void main2(String[] args){
+        boolean[] isPrime = new boolean[1001];
+        for (int i = 2; i <= 1000; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= 1000; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= 1000; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        System.out.println("Простые числа от 1 до 1000:");
+        for (int i = 2; i <= 1000; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
